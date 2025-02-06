0cd358ed7835b18e9e2ae487f94dcbdc149fc31f
Abraham Perez <abraham@Abrahams-MacBook-Pro.local>
2025-02-06 09:35:20 -0600

    adds stepper styles

--- src/components/Inputs/Stepper.tsx
+++ src/components/Inputs/Stepper.tsx
@@ -6,28 +6,34 @@ import { makeStyles } from "@rneui/themed";
 
 type Props = {
   label?: string;
-  onChange: (text: string) => void
+  onChange: (text: string) => void;
 };
 
 const Stepper = ({ label, onChange }: Props) => {
   const [value, setValue] = useState<number>(0);
   const styles = useStyles();
 
-  const handleChange = (num: number) => {
+  const handleAddition = (num: number) => {
     if (value === 0 && num < 0) return;
-    setValue(value => value + num);
-    onChange(value.toFixed(1).toString());
-  }
+    setValue((value) => value + num);
+    onChange(value.toFixed(1).toString())
+  };
 
   return (
     <View style={[globalStyles.row, styles.container]}>
       <Text>{label}</Text>
       <View style={globalStyles.row}>
-        <TouchableOpacity style={[styles.button, styles.buttonMinus]} onPress={() => handleChange(-.5)}>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonMinus]}
+          onPress={() => handleAddition(-0.5)}
+        >
           <Entypo name="minus" size={18} />
         </TouchableOpacity>
-        <Text style={styles.value}>{value.toFixed(1).toString()}</Text>
-        <TouchableOpacity style={[styles.button, styles.buttonPlus]} onPress={() => handleChange(+.5)}>
+        <Text style={styles.value} >{value.toFixed(1).toString()}</Text>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonPlus]}
+          onPress={() => handleAddition(+0.5)}
+        >
           <Entypo name="plus" size={18} color={styles.buttonPlus.color} />
         </TouchableOpacity>
       </View>
@@ -39,26 +45,26 @@ export default Stepper;
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    paddingHorizontal: 5
+    paddingHorizontal: 5,
   },
   button: {
     padding: 12,
     borderRadius: 50,
-    
-    shadowColor: 'rgba(0, 0, 0, 0.5)',
+
+    shadowColor: "rgba(0, 0, 0, 0.5)",
     shadowRadius: 3,
     shadowOpacity: 0.5,
-    shadowOffset : { width: 1, height: 4},
+    shadowOffset: { width: 1, height: 4 },
   },
   buttonMinus: {
     backgroundColor: theme.colors.grey5,
   },
   buttonPlus: {
     backgroundColor: theme.colors.primary,
-    color: theme.colors.grey5
+    color: theme.colors.grey5,
   },
   value: {
-    textAlign: 'center',
-    width: 50
-  }
+    textAlign: "center",
+    width: 50,
+  },
 }));
