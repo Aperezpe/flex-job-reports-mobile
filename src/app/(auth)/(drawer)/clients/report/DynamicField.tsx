3dbaade5d4c78a6870304f13c0931833efbbc490
Abraham <aperezpe01@gmail.com>
2025-04-01 15:22:22 -0500

    job report supports dropdown

--- src/app/(auth)/(drawer)/clients/report/DynamicField.tsx
+++ src/app/(auth)/(drawer)/clients/report/DynamicField.tsx
@@ -4,9 +4,10 @@ import { CustomTextInput } from "../../../../../components/Inputs/CustomInput";
 import { ControllerRenderProps } from "react-hook-form";
 import { FormField } from "../../../../../types/SystemForm";
 import { globalStyles } from "../../../../../constants/GlobalStyles";
+import { CustomDropdown } from "../../../../../components/Inputs/CustomDropdown";
 
 type DynamicFieldProps = {
-  formField: FormField,
+  formField: FormField;
   controllerField: ControllerRenderProps<any, string>;
   errors?: Record<string, any>;
 };
@@ -18,7 +19,9 @@ const DynamicField: React.FC<DynamicFieldProps> = ({
 }) => {
   return (
     <View>
-      <Text style={[globalStyles.textBold, { padding: 5 }]}>{formField.title}</Text>
+      <Text style={[globalStyles.textBold, { paddingBottom: 5 }]}>
+        {formField.title}
+      </Text>
       {formField.type === "text" && (
         <CustomTextInput
           value={controllerField.value}
@@ -28,12 +31,23 @@ const DynamicField: React.FC<DynamicFieldProps> = ({
           inlineErrorMessage={errors?.[`${formField.id}`]?.message}
         />
       )}
-      {formField.type === "date" && <Text>Date Picker</Text>}
-      {formField.type === "dropdown" && <Text>Dropdown Input Component</Text>}
-      {formField.type === "image" && <Text>Image Input</Text>}
-      {!["text", "date", "dropdown", "image"].includes(formField.type ?? '') && (
-        <Text>Unsupported field type</Text>
+      {formField.type === "dropdown" && (
+        <CustomDropdown
+          fieldName={controllerField.name}
+          initialValue={controllerField.value ?? null}
+          onChange={(value) => {
+            controllerField.onChange(value);
+          }}
+          options={formField.content ?? []}
+          inlineErrorMessage={errors?.type?.message}
+          placeholder="Select Option"
+        />
       )}
+      {formField.type === "date" && <Text>Date Picker</Text>}
+      {formField.type === "image" && <Text>Image Input</Text>}
+      {!["text", "date", "dropdown", "image"].includes(
+        formField.type ?? ""
+      ) && <Text>Unsupported field type</Text>}
     </View>
   );
 };
